0749fb84224cd8a9a8980ccbc974ba793aab1610
Mayrasho <mhernandezo1504@alumno.ipn.mx>
2019-11-27 22:37:34 -0600

    ProyectoInicial
    Primer vista del proyecto

---
+++ ProyectoRedes3/Manager/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+def prueba(request):
+    return render (request, "main.html",{"title":"Menu", "year":"2019"})
+
+'''class DeliversClient(View):
+    template_name='deliversClient.html'
+    context_object_name='Delivers'
+    def get(self, request, format=None):
+        result = PaintingRequest.objects.filter(username=request.user.username, status="D").values()
+        return render(request, 
+            self.template_name, 
+            {
+                'result':result,
+                'title':self.context_object_name,
+                'year':datetime.now().year,
+            })'''
